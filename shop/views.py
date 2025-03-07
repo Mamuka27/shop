066ada7adbad07ad3bf35354cc06f75f8e8b71a2
Mamuka27 <mamukagobejishvili3@gmail.com>
2025-03-06 22:43:28 -0800

    Initial commit of Django project (without venv)

---
+++ shop/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'shop/category_list.html', {
+        'categories': categories
+    })
+
+def product_list_by_category(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    products = category.products.all()  # because of related_name='products'
+    return render(request, 'shop/product_list.html', {
+        'category': category,
+        'products': products
+    })
